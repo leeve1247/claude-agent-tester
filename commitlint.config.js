c25b4b3752e9a3304236751af5b8ba4fda41743a
Kihyun <69492686+leeve1247@users.noreply.github.com>
2026-06-17 11:12:36 +0900

    ci: add Angular commit-lint workflow

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+// Angular Commit Convention 검증 규칙의 원천.
+// 허용 type: build, ci, docs, feat, fix, perf, refactor, revert, style, test
+// 헤더 최대 길이 72자, subject 끝 마침표 금지 등은 config-angular가 정의한다.
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+};
